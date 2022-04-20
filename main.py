15c04475eab6ff4667baf80598c7c14daed1ec5f
shivanshu123shrivastav@ <shivanshu123shrivastav@gmail.com>
2022-04-20 21:34:43 +0530

    api updated

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
   
+    
 @app.route('/')
 def hello():
     return "welcome to the flask tutorials"
